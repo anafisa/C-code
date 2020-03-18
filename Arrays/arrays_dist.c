1830a0adb6bf8a040cb8df64d2e87c02d7c0a3d2
Anfisa <anfisa_lepekhina5@mail.ru>
2020-03-18 21:39:34 +0300

    feat: number of different el

--- Arrays/arrays_dist.c
+++ Arrays/arrays_dist.c
@@ -5,24 +5,27 @@
 
 int main()
 {
-    int a[N], b[N];
+    int a[N], b[N], num = 2*N;
     int i, j, d=0;
     srand(time(NULL));
 
+
     for (i = 0; i < N; i++){
-        a[i] = rand()%100;
-        b[i] = rand()%100;
+        a[i] = rand()%10;
+        b[i] = rand()%10;
     }
 
     for (i = 0; i < N; i++){
-        for (j = i; j < N; j++){
-            d++;}
+        for (j = 0; j < N; j++){
+            if (a[i]== b[j])
+                num--;
+        }
     }
-
-    printf("%d", d);
+    printf("%d", num);
     return 0;
 
 }
+
 
 
 
